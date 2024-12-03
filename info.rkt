ec5c705014b0910b78db21f7541884851735e8fd
jaybonthius <46472915+jaybonthius@users.noreply.github.com>
2024-12-03 09:50:28 -0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,22 +1,29 @@
 #lang info
 (define collection "shindig")
-(define deps '("db-lib" "pollen" "sugar" "txexpr" "base" "simple-qr"))
-(define build-deps
-  '("scribble-lib" "racket-doc"
-                   "rackunit-lib"
-                   "reprovide-lang-lib"
-                   "racket-langserver"
-                   "buid"
-                   "data"
-                   "file-watchers"
-                   "dirname"
-                   "http-easy"
-                   "html-parsing"
-                   "pollen"
-                   "db"
-                   "sqlite-table"
-                   "sha"
-                   "uuid"))
+(define deps '(
+    "db-lib"
+    "pollen"
+    "sugar"
+    "txexpr"
+    "base"
+    "simple-qr"
+    "racket-doc"
+    "rackunit-lib"
+    "reprovide-lang-lib"
+    "racket-langserver"
+    "buid"
+    "data"
+    "file-watchers"
+    "dirname"
+    "http-easy"
+    "html-parsing"
+    "pollen"
+    "db"
+    "sqlite-table"
+    "sha"
+    "uuid"
+  ))
+(define build-deps '("scribble-lib"))
 (define scribblings '(("scribblings/shindig.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
